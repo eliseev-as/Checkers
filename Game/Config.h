2a6cdec7dfe2a6213212b09586b7db91ec9874cd
a.eliseev <a.eliseev@equiron.com>
2024-12-12 06:07:20 +0300

    Добавлены комментарии в Config.h

--- Game/Config.h
+++ Game/Config.h
@@ -13,6 +13,7 @@ class Config
         reload();
     }
 
+    // Функция загружает настройки игры из файла конфигурации.
     void reload()
     {
         std::ifstream fin(project_path + "settings.json");
@@ -20,6 +21,7 @@ class Config
         fin.close();
     }
 
+    // Изменяем набор входящих параметров. setting_dir - путь до файла конфигурации. setting_name - имя файла конфигурации.
     auto operator()(const string &setting_dir, const string &setting_name) const
     {
         return config[setting_dir][setting_name];
